aca5cdf6740bba48150fb2bda831c7364ffe7d84
Gabriela Abi Rached Dantas <garacheddantas@gmail.com>
2024-10-25 19:17:58 +0000

    login

--- funcoes.c
+++ funcoes.c
@@ -25,3 +25,31 @@ void cadastrar_usuario() {
     }
 }
 
+// Função para fazer login
+int login_usuario() {
+    Usuario usuario;
+    char nome[50], senha[50]; 
+    FILE *arquivo_bin = fopen(ARQUIVO_USUARIOS_BIN, "rb"); 
+
+    printf("Digite seu nome: ");
+    scanf("%s", nome);
+    printf("Digite sua senha: ");
+    scanf("%s", senha);
+
+    // Verifica se o usuário existe no arquivo binário
+    if (arquivo_bin) {
+        while (fread(&usuario, sizeof(Usuario), 1, arquivo_bin) == 1) {
+            if (strcmp(usuario.nome, nome) == 0 && strcmp(usuario.senha, senha) == 0) {
+                fclose(arquivo_bin);
+                printf("Login bem-sucedido! Bem-vindo, %s!\n", usuario.nome);
+                return 1; 
+            }
+        }
+        fclose(arquivo_bin);
+    } else {
+        printf("Erro ao abrir o arquivo de usuários em binário.\n");
+    }
+
+    printf("Nome ou senha inválidos!\n");
+    return 0; 
+}
